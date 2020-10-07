e8a12a31833cb03a4ce78eb80c4513f1c1d3aed6
DanielaPalacian <38717109+DanielaPalacian@users.noreply.github.com>
2020-10-07 13:15:16 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "kinesis_firehose_stream_name" {
   description = "Name to be use on kinesis firehose stream"
   type        = string
+  default = "default_kinesis_firehose_stream_name"
 }
 
 variable "kinesis_firehose_stream_backup_prefix" {
@@ -18,44 +19,58 @@ variable "root_path" {
 variable "bucket_name" {
   description = "The bucket name"
   type        = string
+  default ="default_bucket_name"
 }
 
 variable "lambda_function_name" {
   description = "The lambda function name"
   type        = string
+  default ="default_lambda_function_name"
 }
 
 variable "lambda_function_file_name" {
   description = "The lambda function file name"
   type        = string
+  default = "default_lambda_function_file_name"
 }
 
 variable "glue_catalog_database_name" {
   description = "The Glue catalog database name"
   type        = string
+   default = "default_glue_catalog_database_name"
 }
 
 variable "glue_catalog_table_name" {
   description = "The Glue catalog database table name"
   type        = string
+   default = "default_glue_catalog_table_name"
 }
 
 variable "glue_catalog_table_columns" {
   description = "A list of table columns"
   type        = list(string)
+   default = {
+     "default_glue_catalog_table_columns1",
+     "default_glue_catalog_table_columns2",
+     "default_glue_catalog_table_columns3",
+     }
+  
 }
 
 variable "cloudwatch_subscription_filter_name" {
   description = "The subscription filter name"
   type        = string
+  default = "default_cloudwatch_subscription_filter_name"
 }
 
 variable "cloudwatch_log_group_name" {
   description = "The cloudwatch log group name"
   type        = string
+  default = "default_cloudwatch_log_group_name"
 }
 
 variable "cloudwatch_filter_pattern" {
   description = "The cloudwatch filter pattern"
   type        = string
+  default = "default_cloudwatch_filter_pattern"
 }
